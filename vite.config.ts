a563f8773518877903a2d56feac21fa8e1203eb8
MrGoofyDuck <mrgoody672@gmail.com>
2025-12-18 06:58:47 +0600

    Working version with fixed images

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/sci-fi-museum/', // ← ВАЖНО
+  base: '/Meseum-/', // ← ВАЖНО
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
